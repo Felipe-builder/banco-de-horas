48eb0ca60ada4e3ebad6ad3d94ff80fa362b40ce
Felipe-builder <felipesvascon@gmail.com>
2022-10-03 20:48:12 -0300

    Conceituando testes

---
+++ index.js
@@ -0,0 +1,14 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.log(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('calculaDescontos', 2200, calculaDescontos(2500, 300));
